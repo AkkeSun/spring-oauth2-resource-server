eb324851b333c25bd6654b7002ed03031f8ba3ce
od <od@sweettracker.co.kr>
2023-04-25 10:24:28 +0900

    feat: MAC 검증 기능 구현 (인증된 객채를 리턴하는 API 추가)

--- src/main/java/com/example/springoauth2resourceserver/controller/ApiController.java
+++ src/main/java/com/example/springoauth2resourceserver/controller/ApiController.java
@@ -1,13 +1,14 @@
 package com.example.springoauth2resourceserver.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 
-    @GetMapping("/api/test")
-    public String test(){
-        return "success";
+    @GetMapping("/api/user")
+    public Authentication user(Authentication authentication) {
+        return authentication;
     }
 }
