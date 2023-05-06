27483010c91b03be3e97c4b521d37af672e9705f
od <od@sweettracker.co.kr>
2023-05-06 13:41:56 +0900

    feat: RSA 검증 기능 구현 (Signer 추가)

--- src/main/java/com/example/springoauth2resourceserver/config/SignatureConfig.java
+++ src/main/java/com/example/springoauth2resourceserver/config/SignatureConfig.java
@@ -1,17 +1,20 @@
 package com.example.springoauth2resourceserver.config;
 
 import com.example.springoauth2resourceserver.signer.MacSecuritySigner;
+import com.example.springoauth2resourceserver.signer.RsaSecuritySigner;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.jwk.OctetSequenceKey;
+import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.gen.OctetSequenceKeyGenerator;
+import com.nimbusds.jose.jwk.gen.RSAKeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SignatureConfig {
 
-    private final String KEY_ID = "macKey";
+    private final String KEY_ID = "myKey";
 
     @Bean
     public MacSecuritySigner macSecuritySigner() {
@@ -25,5 +28,19 @@ public class SignatureConfig {
             .keyID(KEY_ID)
             .algorithm(JWSAlgorithm.HS256)
             .generate();
+    }
+
+    @Bean
+    public RsaSecuritySigner rsaSecuritySigner(){
+        return new RsaSecuritySigner();
+    }
+
+    @Bean
+    public RSAKey rsaKey() throws JOSEException {
+        RSAKey rsaKey = new RSAKeyGenerator(2048)
+            .keyID(KEY_ID)
+            .algorithm(JWSAlgorithm.RS256)
+            .generate();
+        return rsaKey;
     }
 }
---
+++ src/main/java/com/example/springoauth2resourceserver/signer/RsaSecuritySigner.java
@@ -0,0 +1,22 @@
+package com.example.springoauth2resourceserver.signer;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.RSAKey;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+public class RsaSecuritySigner extends SecuritySigner {
+
+    @Override
+    public String getJwtToken(UserDetails user, JWK jwk) {
+        RSASSASigner jwsSigner = null;
+        try {
+            jwsSigner =  new RSASSASigner(((RSAKey)jwk).toRSAPrivateKey());
+        } catch (JOSEException e) {
+            throw new RuntimeException(e);
+        }
+        return super.getJwtTokenInternal(jwsSigner,user,jwk);
+    }
+}
