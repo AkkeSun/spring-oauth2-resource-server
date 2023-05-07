6727151b52db0cb1022a682ebc3cd16aba4f5c29
od <od@sweettracker.co.kr>
2023-05-08 00:16:21 +0900

    feat: scope에 따른 권한 제어 (url 제어)

--- src/main/java/com/example/springoauth2resourceserver/config/OAuth2ResourceServerConfig.java
+++ src/main/java/com/example/springoauth2resourceserver/config/OAuth2ResourceServerConfig.java
@@ -1,83 +1,26 @@
 package com.example.springoauth2resourceserver.config;
 
-import com.example.springoauth2resourceserver.filter.authentication.JwtAuthenticationFilter;
-import com.example.springoauth2resourceserver.filter.authorization.JwtAuthorizationRsaFilter;
-import com.example.springoauth2resourceserver.signer.RsaSecuritySigner;
-import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.crypto.RSASSAVerifier;
-import com.nimbusds.jose.jwk.RSAKey;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 public class OAuth2ResourceServerConfig {
 
-    private final RsaSecuritySigner rsaSecuritySigner;
-    private final RSAKey rsaKey;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests((request) -> request.antMatchers("/").permitAll()
-            .anyRequest().authenticated());
-        http.userDetailsService(userDetailsService());
-        //http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-        // 스프링 시큐리티를 통한 자체 검증에 필요한 설정
+        http.authorizeRequests()
+            .antMatchers("/photos/1").permitAll()
+            .antMatchers("/photos/2").hasAuthority("SCOPE_photo")
+            .anyRequest().authenticated();
         http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.build();
-    }
-
-    // 테스트를 위한 유저 객채 저장
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-            User.withUsername("user").password("1234").authorities("ROLE_USER").build();
-        return new InMemoryUserDetailsManager(user);
-    }
-
-    /*
-    @Bean
-    public JwtAuthorizationMacFilter jwtAuthorizationMacFilter() throws JOSEException {
-        return new JwtAuthorizationMacFilter(new MACVerifier(octetSequenceKey.toSecretKey()));
-    }å
-     */
-
-    @Bean
-    public JwtAuthorizationRsaFilter jwtAuthorizationRsaFilter() throws JOSEException {
-        return new JwtAuthorizationRsaFilter(new RSASSAVerifier(rsaKey.toRSAPublicKey()));
-    }
-
-    @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
-        JwtAuthenticationFilter filter =
-            new JwtAuthenticationFilter(rsaSecuritySigner, rsaKey);
-        filter.setAuthenticationManager(authenticationManager(null));
-        return filter;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
-        throws Exception {
-        return configuration.getAuthenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
     }
 }
--- src/main/java/com/example/springoauth2resourceserver/controller/ApiController.java
+++ src/main/java/com/example/springoauth2resourceserver/controller/ApiController.java
@@ -3,7 +3,6 @@ package com.example.springoauth2resourceserver.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +14,7 @@ public class ApiController {
     // Jwt : 검증에 성공한 JwtToken 의 principal 속성 정보. authentication 에서 꺼낼수도 있다
     public Authentication user(Authentication authentication,
         @AuthenticationPrincipal Jwt principal) {
+        /*
         JwtAuthenticationToken authenticationToken = (JwtAuthenticationToken) authentication;
         String sub = (String) authenticationToken.getTokenAttributes().get("sub");
         String email = (String) authenticationToken.getTokenAttributes().get("email");
@@ -25,6 +25,17 @@ public class ApiController {
         String email1 = principal.getClaim("email");
         String scope1 = principal.getClaim("scope");
         String token = principal.getTokenValue();
+         */
         return authentication;
+    }
+
+    @GetMapping("/photos/1")
+    public String photo1() {
+        return "success";
+    }
+
+    @GetMapping("/photos/2")
+    public String photo2() {
+        return "success";
     }
 }
