3e6003505b2b412b12e2881c686d8e96e30e16e6
od <od@sweettracker.co.kr>
2023-05-07 23:04:49 +0900

    feat: 인증객채 참조를 위한 컨트롤러 수정

--- src/main/java/com/example/springoauth2resourceserver/controller/ApiController.java
+++ src/main/java/com/example/springoauth2resourceserver/controller/ApiController.java
@@ -1,6 +1,9 @@
 package com.example.springoauth2resourceserver.controller;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
 
     @GetMapping("/api/user")
-    public Authentication user(Authentication authentication) {
+    // authentication : 스프링 시큐리티 인증 객채
+    // Jwt : 검증에 성공한 JwtToken 의 principal 속성 정보. authentication 에서 꺼낼수도 있다
+    public Authentication user(Authentication authentication,
+        @AuthenticationPrincipal Jwt principal) {
+        JwtAuthenticationToken authenticationToken = (JwtAuthenticationToken) authentication;
+        String sub = (String) authenticationToken.getTokenAttributes().get("sub");
+        String email = (String) authenticationToken.getTokenAttributes().get("email");
+        String scope = (String) authenticationToken.getTokenAttributes().get("scope");
+        Jwt tokenFromAuthentication = authenticationToken.getToken();
+
+        String sub1 = principal.getClaim("sub");
+        String email1 = principal.getClaim("email");
+        String scope1 = principal.getClaim("scope");
+        String token = principal.getTokenValue();
         return authentication;
     }
 }
